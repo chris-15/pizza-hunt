7dfc66ef9eb1a55d3f2cdf15f17ef36520e1aa64
Chris <christopher.sarm15@gmail.com>
2022-05-18 13:12:43 -0400

    added model validation

--- models/Comment.js
+++ models/Comment.js
@@ -9,11 +9,14 @@ const ReplySchema = new Schema(
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId(),
     },
-    reply: {
+    replyBody: {
       type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
       type: String,
+      required: true
     },
     createdAt: {
       type: Date,
@@ -32,9 +35,12 @@ const CommentSchema = new Schema(
   {
     writtenBy: {
       type: String,
+      required: true
     },
     commentBody: {
       type: String,
+      required: true,
+      trim: true
     },
     createdAt: {
       type: Date,
